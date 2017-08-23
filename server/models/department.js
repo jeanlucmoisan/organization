bbbf71741a70dc324930dd116fbbad4ffb2c5f22
jmoisan <jmoisan@JMOISAN-FR.fr.oracle.com>
2017-08-23 17:51:53 +0200

    add id to department model

--- server/models/department.js
+++ server/models/department.js
@@ -1,12 +1,13 @@
 'use strict';
   class Department {
 	  constructor (object) {
-		this._key = object._key,
-		this.tenant = object.tenant,
-		this.name = object.name,
-		this.collaborators = object.collaborators,
-		this.manager = object.manager,
-		this.addressID = object.addressID
+			this._id = object._id,
+			this._key = object._key,
+			this.tenant = object.tenant,
+			this.name = object.name,
+			this.collaborators = object.collaborators,
+			this.manager = object.manager,
+			this.addressID = object.addressID
 	  }
   }
 module.exports = Department;
